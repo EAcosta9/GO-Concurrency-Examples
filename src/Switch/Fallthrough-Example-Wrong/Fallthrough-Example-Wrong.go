45065889074cb9bf1dd11ca3c68b5195a999fe9c
EAcosta9 <39774790+EAcosta9@users.noreply.github.com>
2020-11-01 15:27:58 -0500

    Create Fallthrough-Example-Wrong.go

---
+++ src/Switch/Fallthrough-Example-Wrong/Fallthrough-Example-Wrong.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Switch Example")
+	num := 1
+	switch num {
+		
+		case 1:
+		fmt.Println("Wound up in case number 1")
+		num = 2
+		
+		case 2:
+		fmt.Println("Wound up in case number 2")
+		num = 3
+		
+		case 3:
+		fmt.Println("Wound up in case number 3")
+	
+	}
+	
+	fmt.Println("I am at the end of this program")
+	
+}
